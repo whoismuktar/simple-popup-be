eabcc7328c446f76cbea5e4ad462ef19e9c74e5b
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-30 16:03:56 +0100

    fix cors, improve response

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+const cors = require("cors");
+app.options("*", cors());
+app.use(cors());
+
 let popUpSettings = {}
 app.get("/popup-settings", (req, res, next) => {
-  res.json(popUpSettings);
+  try {
+    res.status(200).json()({settings: popUpSettings});
+  } catch (error) {
+    res.status(400).json()({message: "There was an error getting settings"});
+  }
 });
 
 app.post("/popup-settings", async(req, res) => {
-  const { settings } = req.body
-
-  popUpSettings = settings
-  res.json({message: "successful", settings: popUpSettings})
+  try {
+    const { settings } = req.body
+  
+    popUpSettings = settings
+    res.status(200).json({message: "successful", settings: popUpSettings})
+  } catch (error) {
+    res.status(400).json()({message: "There was an saving settings"});
+  }
 })
 
 app.get("/", (req, res) => res.type("html").send(html));
