d3d589091b13b9c32de926823787f492205bdd1b
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-30 16:46:39 +0100

    improve api

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const storage = require("node-persist");
+app.use(express.json({ limit: "50mb" }));
 
 const cors = require("cors");
 app.options("*", cors());
 app.use(cors());
 
-let popUpSettings = {}
-app.get("/popup-settings", (req, res, next) => {
+app.get("/popup-settings", async (req, res, next) => {
   try {
-    res.status(200).json()({settings: popUpSettings});
+    await storage.init();
+    const popUpSettings = await storage.getItem("popUpSettings") || {};
+    // if (!popUpSettings) {
+      
+    // }
+
+    console.log({popUpSettings});
+
+    res.status(200).json({ settings: popUpSettings });
   } catch (error) {
-    res.status(400).json()({message: "There was an error getting settings"});
+    res.status(400).json({ message: "There was an error getting settings" });
   }
 });
 
-app.post("/popup-settings", async(req, res) => {
+app.post("/popup-settings", async (req, res) => {
   try {
-    const { settings } = req.body
-  
-    popUpSettings = settings
-    res.status(200).json({message: "successful", settings: popUpSettings})
+    await storage.init();
+    const settings = req.body;
+
+    console.log({settings}, req.body);
+
+    await storage.setItem("popUpSettings", JSON.stringify(settings));
+    const popUpSettings = await storage.getItem("popUpSettings");
+
+    console.log({popUpSettings});
+
+
+    res.status(200).json({ message: "successful", settings: JSON.parse(popUpSettings) });
   } catch (error) {
-    res.status(400).json()({message: "There was an saving settings"});
+    console.log({error});
+    res.status(400).json({ message: "There was an saving settings" });
   }
-})
+});
 
 app.get("/", (req, res) => res.type("html").send(html));
 
-
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 
 const html = `
 <!DOCTYPE html>
@@ -42,4 +57,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
