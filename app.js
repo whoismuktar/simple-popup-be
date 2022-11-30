804b229f187219c5ec75588c76a1a018d0842ed9
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-30 15:34:01 +0100

    init save settings

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+let popUpSettings = {}
+app.get("/popup-settings", (req, res, next) => {
+  res.json(popUpSettings);
+});
+
+app.post("/popup-settings", async(req, res) => {
+  const { settings } = req.body
+
+  popUpSettings = settings
+  res.json({message: "successful", settings: popUpSettings})
+})
+
+app.get("/", (req, res) => res.type("html").send(html));
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
