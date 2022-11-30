7ba5362c78ff6961350eff8b8b3217c30b6d0852
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-30 12:35:04 +0100

    app entry

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <body>
+    <section>
+      Hello from Node!
+    </section>
+  </body>
+</html>
+`
