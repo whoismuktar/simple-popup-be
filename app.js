06079339fcaecf6c601747dd7945eee38640717d
Muktar Yusuf <mayunited12@yahoo.com>
2022-12-03 18:25:45 +0100

    app update

--- app.js
+++ app.js
@@ -3,20 +3,36 @@ const app = express();
 const port = process.env.PORT || 3001;
 const storage = require("node-persist");
 app.use(express.json({ limit: "50mb" }));
+var path = require('path');
+var mime = require('mime');
+var fs = require('fs');
 
 const cors = require("cors");
 app.options("*", cors());
 app.use(cors());
 
+app.get("/cdn", async (req, res, next) => {
+  try {
+      var file = __dirname + '/assets/popup.js';
+    
+      var filename = path.basename(file);
+      var mimetype = mime.lookup(file);
+    
+      res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+      res.setHeader('Content-type', mimetype);
+    
+      var filestream = fs.createReadStream(file);
+      filestream.pipe(res);
+    } catch (error) {
+    console.log({error});
+    res.status(400).json({ message: "There was an error getting settings" });
+  }
+});
+
 app.get("/popup-settings", async (req, res, next) => {
   try {
     await storage.init();
-    const popUpSettings = await storage.getItem("popUpSettings") || {};
-    // if (!popUpSettings) {
-      
-    // }
-
-    console.log({popUpSettings});
+    const popUpSettings = (await storage.getItem("popUpSettings")) || {};
 
     res.status(200).json({ settings: popUpSettings });
   } catch (error) {
@@ -29,24 +45,20 @@ app.post("/popup-settings", async (req, res) => {
     await storage.init();
     const settings = req.body;
 
-    console.log({settings}, req.body);
-
     await storage.setItem("popUpSettings", JSON.stringify(settings));
     const popUpSettings = await storage.getItem("popUpSettings");
 
-    console.log({popUpSettings});
-
-
-    res.status(200).json({ message: "successful", settings: JSON.parse(popUpSettings) });
+    res
+      .status(200)
+      .json({ message: "successful", settings: JSON.parse(popUpSettings) });
   } catch (error) {
-    console.log({error});
     res.status(400).json({ message: "There was an saving settings" });
   }
 });
 
 app.get("/", (req, res) => res.type("html").send(html));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}!`));
 
 const html = `
 <!DOCTYPE html>
